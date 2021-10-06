5e165782dd2012132cbfea5968dc256e435ab241
Rajesh <htcuser@CH1151.local>
2021-10-06 16:08:28 +0530

    Added SegmentControl ForYou,Following

--- TikTok/Controllers/Core/HomeViewController.swift
+++ TikTok/Controllers/Core/HomeViewController.swift
@@ -9,11 +9,184 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let horizontalScrollView: UIScrollView = {
+        
+        let scrollView = UIScrollView()
+        scrollView.bounces = false
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        return scrollView
+    }()
+    
+    let control: UISegmentedControl = {
+        
+        let titles = ["Following","For You"]
+        let control = UISegmentedControl(items: titles)
+        
+        control.selectedSegmentIndex = 1
+        control.backgroundColor = nil
+        control.selectedSegmentTintColor = .white
+        return control
+    }()
+    
+    let forYouPageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .vertical, options: [:])
+    
+    let followingPageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .vertical, options: [:])
+    
+    
+    private var forYouPosts = PostModel.mockModels()
+    private var followingPost = PostModel.mockModels()
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(horizontalScrollView)
+        
+        setUpFeed()
+        horizontalScrollView.delegate = self
+        horizontalScrollView.contentOffset = CGPoint(x: view.width, y: 0)
+        setUpHeaderButtons()
     }
 
-
+    
+    override func viewDidLayoutSubviews() {
+        
+        super.viewDidLayoutSubviews()
+        horizontalScrollView.frame = view.bounds
+    }
+    
+    
+    func setUpHeaderButtons() {
+        
+        control.addTarget(self, action: #selector(didChangeSegmentedControl(_:)), for: .valueChanged)
+        navigationItem.titleView = control
+    }
+    
+    @objc private func didChangeSegmentedControl(_ sender: UISegmentedControl) {
+        
+        horizontalScrollView.setContentOffset(CGPoint(x: view.width * CGFloat(sender.selectedSegmentIndex), y: 0), animated: false)
+    }
+    
+    private func setUpFeed() {
+        
+        horizontalScrollView.contentSize = CGSize(width: view.width * 2, height: view.height)
+        
+        setUpFollowingFeed()
+        setUpForYouFeed()
+    }
+    
+    func setUpFollowingFeed() {
+        
+        guard let model = followingPost.first else {
+            
+            return
+        }
+        
+        followingPageViewController.setViewControllers([PostViewController(model: model)], direction: .forward, animated: false, completion: nil)
+        
+        followingPageViewController.dataSource = self
+        
+        horizontalScrollView.addSubview(followingPageViewController.view)
+        followingPageViewController.view.frame = CGRect(x: 0, y: 0, width: horizontalScrollView.width, height: horizontalScrollView.height)
+        
+        addChild(followingPageViewController)
+        followingPageViewController.didMove(toParent: self)
+    }
+    
+    func setUpForYouFeed()  {
+        
+        guard let model = forYouPosts.first else {
+            
+            return
+        }
+        
+        forYouPageViewController.setViewControllers([PostViewController(model: model)], direction: .forward, animated: false, completion: nil)
+        
+        forYouPageViewController.dataSource = self
+        
+        horizontalScrollView.addSubview(forYouPageViewController.view)
+        forYouPageViewController.view.frame = CGRect(x: view.width, y: 0, width: horizontalScrollView.width, height: horizontalScrollView.height)
+        
+        addChild(forYouPageViewController)
+        forYouPageViewController.didMove(toParent: self)
+    }
 }
 
+extension HomeViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let fromPost = (viewController as? PostViewController)?.model else {
+            
+            return nil
+        }
+        
+        guard let index = currentPosts.firstIndex(where: {$0.identifier == fromPost.identifier}) else {
+            
+            return nil
+        }
+        
+        if index == 0 {
+            
+            return nil
+        }
+        
+        let priorIndex = index - 1
+        let model = currentPosts[priorIndex]
+        let vc = PostViewController(model: model)
+        
+        return vc
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let fromPost = (viewController as? PostViewController)?.model else {
+            
+            return nil
+        }
+        
+        guard let index = currentPosts.firstIndex(where: {$0.identifier == fromPost.identifier}) else {
+            
+            return nil
+        }
+        
+        guard index < (currentPosts.count - 1) else {
+            return nil
+        }
+        let nextIndex = index + 1
+        let model = currentPosts[nextIndex]
+        let vc = PostViewController(model: model)
+        
+        return vc
+    }
+    
+    var currentPosts: [PostModel] {
+        
+        if horizontalScrollView.contentOffset.x == 0  {
+            
+            //Following
+            
+            return followingPost
+        }
+        
+        // ForYou
+        return forYouPosts
+    }
+    
+}
+
+
+extension HomeViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.x == 0 || scrollView.contentOffset.x <= (view.width/2){
+            control.selectedSegmentIndex = 0
+        }
+        else if scrollView.contentOffset.x > (view.width/2) {
+            control.selectedSegmentIndex = 1
+        }
+    }
+}
---
+++ TikTok/Models/PostModel.swift
@@ -0,0 +1,27 @@
+//
+//  PostModel.swift
+//  TikTok
+//
+//  Created by Rajesh on 06/10/21.
+//
+
+import Foundation
+
+struct PostModel {
+    
+    let identifier: String
+    
+    static func mockModels() -> [PostModel] {
+        
+        var posts = [PostModel]()
+        
+        for _ in 0...100 {
+            
+            let post = PostModel(identifier: UUID().uuidString)
+            posts.append(post)
+            
+        }
+        
+        return posts
+    }
+}
